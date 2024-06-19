ca934c479c7861fe3ab87fad0f2e7591042fbd69
Grey <grey@Fuzzys-MacBook-Pro.local>
2024-06-18 17:57:23 -0700

    rm

--- next.config.js
+++ next.config.js
@@ -7,17 +7,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  env: {
-    NEAR_ACCOUNT_ID: "gregx.testnet",
-    NEAR_PRIVATE_KEY: "ed25519:5FUUm31hdnh5wnahfh932MaZEmzKVeHyF6ckviss4f1HkAPMWDqooxbPTQcBHFF8mqyTak4qCa2b383mt5542amS",
-    MPC_PATH: "ethereum,1",
-    MPC_CHAIN: "[ethereum]",
-    MPC_CONTRACT_ID: "v2.multichain-mpc.testnet",
-    MPC_PUBLIC_KEY: "secp256k1:4NfTiv3UsGahebgTaHyD9vF8KYKMBnfd6kh94mK6xv8fGBiJB8TBtFMP5WWXz6B89Ac1fbpzPwAvoyQebemHFwx3",
-    TATUM_API_KEY: "t-660377bd70604e001c3712e6-045c28a4cd40450e890ecded",
-    NEAR_PROXY: "false"
-  }
+  basePath: ''
 };
 
 module.exports = nextConfig;
