16bdf94f9a69ff8099fb2edabf236196fee0bbc4
Grey <grey@Fuzzys-MacBook-Pro.local>
2024-06-18 18:04:01 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -7,7 +7,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: ''
+  basePath: '',
+  env: {
+    MPC_CONTRACT_ID: "v2.multichain-mpc.testnet",
+    MPC_PUBLIC_KEY: "secp256k1:4NfTiv3UsGahebgTaHyD9vF8KYKMBnfd6kh94mK6xv8fGBiJB8TBtFMP5WWXz6B89Ac1fbpzPwAvoyQebemHFwx3",
+    TATUM_API_KEY: "t-660377bd70604e001c3712e6-045c28a4cd40450e890ecded",
+  }
 };
 
 module.exports = nextConfig;
